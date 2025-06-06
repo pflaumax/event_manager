c125fc652796e1bb33188fe0894e9e03cb1cfcbf
pflaumax <pflaumax@gmail.com>
2025-06-06 16:41:42 +0300

    add home page render

--- apps/events/views.py
+++ apps/events/views.py
@@ -2,7 +2,6 @@ from django.http import Http404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.http import require_POST
 from .models import Event, EventRegistration
 from .forms import EventForm
 
