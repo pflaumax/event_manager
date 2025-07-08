cbfbc6a773ffbfec79dd15701f1ced9a90354d4e
pflaumax <pflaumax@gmail.com>
2025-07-08 18:15:12 +0300

    refactor urls: reorder urlpatterns for improved readability and organization

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    # Include default auth urls (login)
-    path("", include("django.contrib.auth.urls")),
+    # Login page
+    path("login/", views.login_view, name="login"),
     # Registration page
     path("sign_up/", views.signup, name="signup"),
+    # Include default auth urls (login)
+    path("", include("django.contrib.auth.urls")),
     # Account activation
     path("activate/<uidb64>/<token>/", views.activate, name="activate"),
 ]
